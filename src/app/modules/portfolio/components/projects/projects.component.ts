13f04fd10a3c9d548ee8e43fbb94b3549520a821
AnaInsfran <anainsfran@gmail.com>
2024-03-11 20:40:08 -0300

    feature: CSS projects component

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -26,6 +26,76 @@ export class ProjectsComponent {
           href: 'https://vidafullstack.com.br',
         }
       ]
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida Fullstack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p><p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida Fullstack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p><p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida Fullstack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p><p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida Fullstack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p><p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
+    },
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida Fullstack',
+      title: 'Vida Fullstack',
+      width: '100px',
+      height: '51px',
+      description: '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p><p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        }
+      ]
     }
   ])
 }
